b3ef8c8dccb6e95f28423cccbeb605da3a3f0bb7
res09ggm <res09ggm@gmail.com>
2017-10-06 18:28:41 -0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
+. /opt/miner/miner.conf
 
-for file in ../bin/*; do
-    filename=`basename $file`
-    sudo ln -s $file /usr/local/bin/$filename
-done;
+echo "Continue with setup? [y/n]"
+read answer
+if echo $answer | grep -iq "^n"; then
+    exit 1
+fi
+
+if [ -z `echo $PATH | grep /opt/miner/bin` ]; then
+    sudo bash -c 'echo "PATH=${PATH}:/opt/miner/bin" >> /etc/.profile'
+    . /etc/.profile
+fi
+
+echo "Set worker name? ${WORKER_NAME}: [y/n]"
+read answer
+if echo $answer | grep -iq "^y"; then
+    echo -n "${WORKER_NAME}: "
+    read answer
+    TMP_WORKER_NAME=$answer
+fi
+
+echo "Set worker id? ${WORKER_ID}: [y/n]"
+read answer
+if echo $answer | grep -iq "^y"; then
+    echo -n "${WORKER_ID}: "
+    read answer
+    TMP_WORKER_ID=$answer
+fi
