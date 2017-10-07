1907514a151457e96d50d2c19c6f3ef1aa1ca761
res09ggm <res09ggm@gmail.com>
2017-10-06 17:43:50 -0700

    Update restart_mining.sh

--- scripts/restart_mining.sh
+++ scripts/restart_mining.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+[ -e /opt/miner/miner.conf ] && . /opt/miner/miner.conf
 
 sudo pkill -f -9 ethminer
 
-sudo /usr/local/bin/resetgpu.sh
-sudo /usr/local/bin/setgpu.sh
+/usr/local/bin/resetgpu.sh
+/usr/local/bin/setgpu.sh
 /usr/local/bin/ethermine.sh
