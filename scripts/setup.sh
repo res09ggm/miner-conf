a0f723bf9f51dc3c4ebbb2a98238878b41aad373
res09ggm <res09ggm@gmail.com>
2017-10-06 19:25:55 -0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -44,7 +44,7 @@ fi
 echo "Set hostname from ${HOSTNAME} to ${WORKER_NAME}-${WORKER_ID}?"
 read answer
 if echo $answer | grep -iq "^y"; then
-    sudo sed -i "s/^${HOSTNAME}/${WORKER_NAME}-${WORKER_ID}/" /etc/hosts
+    sudo sed -i "s/${HOSTNAME}/${WORKER_NAME}-${WORKER_ID}/" /etc/hosts
     sudo hostnamectl set-hostname "${WORKER_NAME}${WORKER_ID}"
 fi
 
