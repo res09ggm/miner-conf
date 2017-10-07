bc249ab59dd89979d600a49ae7d73f9625e628e2
res09ggm <res09ggm@gmail.com>
2017-10-06 17:38:24 -0700

    Update resetgpu.sh

--- scripts/resetgpu.sh
+++ scripts/resetgpu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+[ -e /opt/miner/miner.conf ] && . /opt/miner/miner.conf
 GPU_CLOCK_OFFSET=0
 GPU_MEM_OFFSET=0
 
-for i in `seq 0 12`;
+for i in `seq 0 $((GPU_COUNT - 1))`;
 do
-sudo nvidia-settings -a [gpu:$i]/GPUGraphicsClockOffset[3]=$GPU_CLOCK_OFFSET -a [gpu:$i]/GPUMemoryTransferRateOffset[3]=$GPU_MEM_OFFSET -a [gpu:$i]/GPUFanControlState=0
+    sudo nvidia-settings -a [gpu:$i]/GPUGraphicsClockOffset[2]=0 -a [gpu:$i]/GPUMemoryTransferRateOffset[2]=0 -a [gpu:$i]/GPUFanControlState=0
 done;
