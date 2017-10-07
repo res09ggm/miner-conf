748852e02ce725624f24b214f4e638f080ab5bfa
res09ggm <res09ggm@gmail.com>
2017-10-06 17:40:20 -0700

    Update setgpu.sh

--- scripts/setgpu.sh
+++ scripts/setgpu.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
-GPU_CLOCK_OFFSET=150
-GPU_MEM_OFFSET=1450
-sudo nvidia-smi -pm 1
-sudo nvidia-smi -pl 80
-for i in `seq 0 12`;
-do
-sudo nvidia-settings -a [gpu:$i]/GPUGraphicsClockOffset[2]=$GPU_CLOCK_OFFSET -a [gpu:$i]/GPUMemoryTransferRateOffset[2]=$GPU_MEM_OFFSET -a [gpu:$i]/GPUFanControlState=1 -a [fan:$i]/GPUTargetFanSpeed=85
-done;
+#!/usr/bin/env bash
+[ -e /opt/miner/miner.conf ] && . /opt/miner/miner.conf
 
-#sudo nvidia-settings -a [gpu:4]/GPUGraphicsClockOffset[2]=100 -a [gpu:4]/GPUMemoryTransferRateOffset[2]=1300
+sudo nvidia-smi -pm 1
+sudo nvidia-smi -pl ${GPU_POWER_LIMIT}
+
+for i in `seq 0 $((GPU_COUNT - 1))`;
+do
+  sudo nvidia-settings \
+  -a [gpu:$i]/GPUGraphicsClockOffset[2]=${GPU_CORE_CLOCK} \
+  -a [gpu:$i]/GPUMemoryTransferRateOffset[2]=${GPU_MEM_CLOCK} \
+  -a [gpu:$i]/GPUFanControlState=1 \
+  -a [fan:$i]/GPUTargetFanSpeed=${GPU_FAN_SPEED}
+done;
